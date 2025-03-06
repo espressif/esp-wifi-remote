6c6758402c2218c643c5c711f7150a1905aaa842
David Cermak <cermak@espressif.com>
2025-03-06 17:26:51 +0100

    feat(wifi_remote): Add target test with esp_hosted

--- components/esp_wifi_remote/examples/pytest_eppp.py
+++ components/esp_wifi_remote/examples/pytest_eppp.py
@@ -10,13 +10,17 @@ from pytest_embedded_idf.dut import IdfDut
 @pytest.mark.parametrize(
     'count, app_path, target', [
         (2,
-         f'{os.path.join(os.path.dirname(__file__), "mqtt")}|{os.path.join(os.path.dirname(__file__), "server")}',
-         'esp32p4|esp32c6'),
+         f'{os.path.join(os.path.dirname(__file__), "server")}|{os.path.join(os.path.dirname(__file__), "mqtt")}',
+         'esp32c6|esp32p4'),
     ], indirect=True
 )
 def test_wifi_remote_eppp(dut: Tuple[IdfDut, IdfDut]) -> None:
-    client = dut[0]
-    server = dut[1]
+    server = dut[0]
+    client = dut[1]
 
-    server.expect('rpc_server: Received WIFI event 4', timeout=100)     # wifi station connected
+    # Check for wifi station connected event (different for hosted and eppp)
+    if dut[1].app.sdkconfig.get('ESP_WIFI_REMOTE_LIBRARY_HOSTED') is True:
+        server.expect('slave_rpc: Sta mode connected', timeout=100)
+    else:
+        server.expect('rpc_server: Received WIFI event 4', timeout=100)
     client.expect('MQTT_EVENT_CONNECTED', timeout=100)
